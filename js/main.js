a4e11aa1289ff0ec6f60edd57f3f36048ff92537
ApsaraWitharana <asachini095@gmail.com>
2024-08-07 16:16:38 +0530

    update-frantend save,update,delete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,92 @@
 $('#savepost').click( function (){
-    console.log("event is triggered!!")
+    //catch the input fealde  values
+   let  postId = $('#post-id').val(); // get value
+    let  postTitle = $('#post-title').val(); // get value
+    let  postCategory = $('#post-category').val(); // get value
+    let  text= $('#text').val(); // get value
+    console.log(postId,postTitle,postCategory,text);
+
+    $.ajax({
+        url: "http://localhost:8080/blog/savepost",
+        method:"POST",
+        contentType:"application/json",
+        "data":JSON.stringify({
+            "id":postId,
+            "title":postTitle,
+            "category":postCategory,
+            "content":text
+        }),
+        success:function (result){
+            console.log(result)
+            alert("done")
+        },
+        error:function (error){
+            console.log(error)
+            alert("try again")
+        }
+    })
+
 })
+
+$('#deletepost').click( function (){
+    let  postId = $('#post-id').val(); // get value
+    $.ajax({
+        url: "http://localhost:8080/blog/deletepost/"+postId, // Include postId in the URL
+        method: "DELETE",
+        // contentType: "application/json",
+        // dataType: "json", // Expect JSON response
+        success: function (result) {
+            console.log(result);
+            alert("delete successful");
+        },
+        error: function (error) {
+            console.log(error);
+            alert("delete failed, please try again");
+        }
+    });
+});
+
+$('#getpost').click(function (){
+    let  postId = $('#post-id').val(); // get value
+    $.ajax({
+         url: "http://localhost:8080/blog/getpost/" + postId, // Include postId in the URL
+        method: "GET",
+        success: function (result) {
+            console.log(result);
+            alert("get successful");
+        },
+        error: function (error) {
+            console.log(error);
+            alert("get failed, please try again");
+        }
+    });
+})
+$('#updatepost').click(function () {
+    // Catch the input field values
+    let postId = $('#post-id').val(); // Get value
+    let postTitle = $('#post-title').val(); // Get value
+    let postCategory = $('#post-category').val(); // Get value
+    let text = $('#text').val(); // Get value
+    console.log(postId, postTitle, postCategory, text);
+
+    $.ajax({
+        url: "http://localhost:8080/blog/updatepost ",
+        method: "PUT",
+        contentType: "application/json",
+        dataType: "json", // Expect JSON response
+        data: JSON.stringify({
+            "id":postId,
+            "title":postTitle,
+            "category":postCategory,
+            "content":text
+        }),
+        success: function (result) {
+            console.log(result);
+            alert("Update successful");
+        },
+        error: function (error) {
+            console.log(error);
+            alert("Update failed, please try again");
+        }
+    });
+});
